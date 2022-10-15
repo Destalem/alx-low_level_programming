df0ed7a11cc63a79595744b72d32b7e9a52ec806
Destalem <Destalema9@gmail.com>
2022-10-15 04:02:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,8 +7,7 @@
 int main(void)
 {
 	int num1, num2;
-	for (num1 = 0; num1 <= 0; num1++)
-	{
+	num1 =0;
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
@@ -21,7 +20,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-	}
+	
 	putchar('\n');
 	return (0);
 }
