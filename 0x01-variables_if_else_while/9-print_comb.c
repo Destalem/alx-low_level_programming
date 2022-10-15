1556db9ce1e5f9ee47dd1cbafceea59837a57a1c
Destalem <Destalema9@gmail.com>
2022-10-15 02:34:36 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
+	{
 		putchar(num);
 	if (num == 9)
-	{
-		break;
-	}
+		continue;
 	putchar(',');
 	putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
