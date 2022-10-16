4f27bfb2c0ed78be60baf7df247a6711ee8fcfff
Destalem <Destalema9@gmail.com>
2022-10-16 01:18:08 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,8 +15,8 @@ int main(void)
 			putchar((num1 / 100) + '0');
 			putchar((num2 / 100) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((num2 / 100) + '0');
+			putchar((num2 % 100) + '0');
 			if ((num1 == 98) && (num2 == 99))
 				continue;
 			putchar(',');
