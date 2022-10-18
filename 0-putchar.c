f627b033315d735fa74eef43e6b6207bb86107ba
Destalem <Destalema9@gmail.com>
2022-10-18 08:55:40 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[10] = "_putchar";
+	
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar(10);
+	return (0);
+}
+
