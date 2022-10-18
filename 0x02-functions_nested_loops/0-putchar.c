0ee2036af8dbd2020c149f9f91a3591c7a75e9bd
Destalem <Destalema9@gmail.com>
2022-10-18 09:02:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[10] = "_putchar";
+
+	itn i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar(10);
+	return (0);
+}
